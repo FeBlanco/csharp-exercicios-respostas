d0f12767d2b6156a195ee45e6a54ac59a6949331
Felipe Blanco <feeh.blanco@gmail.com>
2024-04-11 16:49:26 -0300

    Exercícios

---
+++ ExercicioColecao/01Exercicio/Program.cs
@@ -0,0 +1,53 @@
+﻿Console.WriteLine("\n### 01 - Exercício coleção com nome de frutas ###\n");
+
+//Declarando e inicializa o array de strings com nome de frutas
+string[] frutas = {
+"Maça", "Banana", "Laranja", "Uva", "Manga", "Pêra", "Abacate", "Mamão", "Pêssego", "Amora"
+};
+
+//laço foreach
+Console.WriteLine("\n\nlaço foreach");
+foreach (var fruta in frutas)
+{
+    Console.Write($"{fruta} ");
+}
+
+//laço for
+Console.WriteLine("\n\nlaço for");
+for (int i = 0; i < frutas.Length; i++)
+{
+    Console.Write($"{frutas[i]} ");
+}
+Console.WriteLine($"\n\nA coleção frutas possui : {frutas.Count()} frutas");
+
+//exibe a segunda e penúltima fruta
+Console.WriteLine("\nNome da segunda e penúltima fruta");
+Console.WriteLine($"\n Segunda   =>  {frutas[1]}");
+Console.WriteLine($"\n Penúltima =>  {frutas[8]}");
+
+//altera a terceira e última fruta
+Console.WriteLine("\nAlterando o nome da terceira para Kiwi e da última fruta para Caqui...");
+frutas[2] = "Kiwi";
+frutas[9] = "Caqui";
+
+Console.WriteLine("\n\nRelação de frutas com nomes alterados: \n");
+foreach (var fruta in frutas)
+{
+    Console.Write($"{fruta} ");
+}
+
+Console.WriteLine("\n\nOrdenando a coleção de frutas com Array.Sort()");
+Array.Sort(frutas);
+foreach (var fruta in frutas)
+{
+    Console.Write($"{fruta} ");
+}
+
+Console.WriteLine("\n\nInvertendo a ordem da coleção");
+Array.Reverse(frutas);
+foreach (var fruta in frutas)
+{
+    Console.Write($"{fruta} ");
+}
+
+Console.ReadKey();
