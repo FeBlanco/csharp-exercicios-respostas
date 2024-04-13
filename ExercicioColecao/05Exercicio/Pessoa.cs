971117b939e75c62ba7e713d06e64200dffdb3d9
Felipe Blanco <feeh.blanco@gmail.com>
2024-04-13 02:08:57 -0300

    Exercícios

---
+++ ExercicioColecao/05Exercicio/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05Exercicio;
+
+internal class Pessoa
+{
+    //a-) Primeiro crie uma classe chamada Pessoa.cs com duas propriedades (nome e idade) e um método Exibir() 
+    //para imprimir o nome e a idade;
+    public string? Nome { get; set; }
+    public int Idade { get; set; }
+
+    public string Exibir()
+    {
+        return Nome + " - " + Idade;
+    }
+}
---
+++ ExercicioColecao/05Exercicio/Program.cs
@@ -0,0 +1,49 @@
+﻿using _05Exercicio;
+using System.Collections;
+
+Console.WriteLine("## 04 - Exercício ArrayList ##\n");
+
+ArrayList lista = new ArrayList();
+
+Console.WriteLine("\nInforme o nome e idade de 3 pessoas...\n");
+int total = 3;
+for (int i = 0; i < total; i++)
+{
+    Console.Write("\nNome : ");
+    string nome = Console.ReadLine();
+    Console.Write("Idade : ");
+    int idade = int.Parse(Console.ReadLine());
+
+    lista.Add(new Pessoa()
+    {
+        Nome = nome,
+        Idade = idade
+    });
+}
+
+ListaPessoas(lista);
+
+Console.WriteLine("\nIncluindo mais duas pessoas na lista...");
+
+Pessoa jaime = new Pessoa() { Nome = "Jaime", Idade = 20 };
+Pessoa tania = new Pessoa() { Nome = "Tânia", Idade = 18 };
+
+lista.Add(jaime);
+lista.Add(tania);
+
+ListaPessoas(lista);
+
+Console.WriteLine("\nRemovendo o último elemento...");
+lista.RemoveAt(lista.Count - 1);
+ListaPessoas(lista);
+
+Console.ReadKey();
+
+static void ListaPessoas(ArrayList lista)
+{
+    Console.WriteLine("\nLista de pessoas atual...\n");
+    foreach (Pessoa pessoa in lista)
+    {
+        Console.WriteLine(pessoa.Exibir());
+    }
+}
